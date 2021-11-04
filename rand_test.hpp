4d8bd4222d9264c953ba2e8c4241d8b66e266ace
kristianjordan <krisjord24@gmail.com>
2021-11-03 22:46:57 -0700

    Finished implementation and testing for rand class

--- rand_test.hpp
+++ rand_test.hpp
@@ -6,44 +6,20 @@
 #include "rand.hpp"
 using namespace std;
 
-class OneRandMock: public Base {
-    public:
-        OneRandMock() { }
-
-        virtual double evaluate() { return 1.0; }
-        virtual string stringify() { return "1.0"; }
-};
-
-class FiftyRandMock: public Base {
-    public:
-        FiftyRandMock() { }
-
-        virtual double evaluate() { return 50.0; }
-        virtual string stringify() { return "50.0"; }
-};
-
-class HundredRandMock: public Base {
-    public:
-        HundredRandMock() { }
-
-        virtual double evaluate() { return 100.0; }
-        virtual string stringify() { return "100.0"; }
-};
-
-
-TEST(RandTest, OneRandMockTest) {
-    OneRandMock* test = new OneRandMock();
-    EXPECT_DOUBLE_EQ(test->evaluate(), 1.0);
+// real rand test
+TEST(RandTest, RandGreaterOrEqualOne) {
+    Rand *test = new Rand();
+    double randomNumber = test->evaluate(); // set variable equal to random generated number
+    EXPECT_GE(randomNumber, 1);
+    EXPECT_TRUE(test->stringify() == to_string(randomNumber));
+	
 }
 
-TEST(RandTest, FiftyRandMockTest) {
-    FiftyRandMock* test = new FiftyRandMock();
-    EXPECT_DOUBLE_EQ(test->evaluate(), 50.0);
-}
-
-TEST(RandTest, HundredRandMockTest) {
-    HundredRandMock* test = new HundredRandMock();
-    EXPECT_DOUBLE_EQ(test->evaluate(), 100.0);
+TEST(RandTest, RandLessEqualHundred) {
+    Rand *test = new Rand();
+    double randomNumber = test->evaluate(); // set variable equal to random generated number
+    EXPECT_LE(randomNumber, 100);
+    EXPECT_TRUE(test->stringify() == to_string(randomNumber));
 }
 
 #endif //__RAND_TEST_HPP__
