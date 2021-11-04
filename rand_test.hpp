07965696eccafc242bce1b37c6cf060b3a3058e6
kristianjordan <krisjord24@gmail.com>
2021-11-03 23:08:00 -0700

    Adding test to rand_test.hpp

--- rand_test.hpp
+++ rand_test.hpp
@@ -4,9 +4,25 @@
 #include "gtest/gtest.h"
 #include <string>
 #include "rand.hpp"
+#include "mult.hpp"
 using namespace std;
 
 // real rand test
+
+TEST(RandTest, RandMultTest) {
+    Rand *test = new Rand();
+    Op * ten = new Op(10);
+    double randomNumber = test->evaluate(); // set variable equal to random generated number
+    Op * random = new Op(randomNumber); // create new op object and pass in random number to use in 
+					// multiplication
+    Mult * multiplication = new Mult(random, ten);
+    EXPECT_GE(randomNumber, 1);
+    EXPECT_TRUE(multiplication->stringify() == ("(" + to_string(randomNumber) + " * " + to_string(10.000000) + ")"));
+	
+	
+}
+
+
 TEST(RandTest, RandGreaterOrEqualOne) {
     Rand *test = new Rand();
     double randomNumber = test->evaluate(); // set variable equal to random generated number
