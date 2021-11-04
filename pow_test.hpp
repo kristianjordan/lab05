06943762ffa97715ef975b438e0b1e5c2ed5e955
kristianjordan <krisjord24@gmail.com>
2021-11-03 23:28:48 -0700

    Finished implementation and testing for pow class

--- pow_test.hpp
+++ pow_test.hpp
@@ -4,46 +4,47 @@
 #include "gtest/gtest.h"
 #include <string>
 #include "pow.hpp"
+#include "mult.hpp"
 using namespace std;
 
-class PositivePowMock: public Base {
-    public:
-        PositivePowMock() { }
+// (1 ^ 9) * 74
+TEST(PowTest, PowMultCombinedTest) {
+        Base *operand1 = new Op(1);
+        Base *operand2 = new Op(9);
+        Base *operand3 = new Op(74);
+        Base *pow = new Pow(operand1, operand2);
+        Base *multiplication = new Mult(pow, operand3);
 
-        virtual double evaluate() { return 9.0; }
-        virtual string stringify() { return "9.0"; }
-};
-
-class ZeroPowMock: public Base {
-    public:
-        ZeroPowMock() { }
-
-        virtual double evaluate() { return 0.0; }
-        virtual string stringify() { return "0.0"; }
-};
-
-class NegativePowMock: public Base {
-    public:
-        NegativePowMock() { }
-
-        virtual double evaluate() { return -1.0; }
-        virtual string stringify() { return "-1.0"; }
-};
-
-
-TEST(PowTest, PositivePowMockTest) {
-    PositivePowMock* test = new PositivePowMock();
-    EXPECT_DOUBLE_EQ(test->evaluate(), 9.0);
+        EXPECT_DOUBLE_EQ(multiplication->evaluate(), 74.0);
+        EXPECT_TRUE(multiplication->stringify() == "((1.000000 ^ 9.000000) * 74.000000)");
 }
 
-TEST(PowTest, ZeroMultMockTest) {
-    ZeroPowMock* test = new ZeroPowMock();
-    EXPECT_DOUBLE_EQ(test->evaluate(), 0.0);
+
+TEST(PowTest, PowTestPositiveNumber) {
+        Op *operand1 = new Op(1);
+        Op *operand2 = new Op(35);
+        Pow *pow = new Pow(operand1, operand2);
+
+        EXPECT_DOUBLE_EQ(pow->evaluate(), 1.0);
+        EXPECT_TRUE(pow->stringify() == "(1.000000 ^ 35.000000)");
 }
 
-TEST(PowTest, NegativePowMockTest) {
-    NegativePowMock* test = new NegativePowMock();
-    EXPECT_DOUBLE_EQ(test->evaluate(), -1.0);
+TEST(PowTest, PowTestZero) {
+        Op *operand1 = new Op(0);
+        Op *operand2 = new Op(13);
+        Pow *pow = new Pow(operand1, operand2);
+
+        EXPECT_DOUBLE_EQ(pow->evaluate(), 0.0);
+        EXPECT_TRUE(pow->stringify() == "(0.000000 ^ 13.000000)");
+}
+
+TEST(PowTest, PowNegativeNumber) {
+        Op *operand1 = new Op(-1);
+        Op *operand2 = new Op(3);
+        Pow *pow = new Pow(operand1, operand2);
+
+        EXPECT_DOUBLE_EQ(pow->evaluate(), -1.0);
+        EXPECT_TRUE(pow->stringify() == "(-1.000000 ^ 3.000000)");
 }
 
 #endif //__POW_TEST_HPP__
