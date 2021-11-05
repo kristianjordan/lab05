a0e44c0f667fc11548eff1dc734d65434baccc29
kristianjordan <krisjord24@gmail.com>
2021-11-05 12:14:22 -0700

    Updating and adding test case

--- rand_test.hpp
+++ rand_test.hpp
@@ -20,7 +20,7 @@ TEST(RandTest, RandMultTest) {
 					// multiplication
 					//
     Mult * multiplication = new Mult(random, ten); // create mult object to pass operands and assign to pointer
-    EXPECT_GE(randomNumber, 1); // expect random number to be >= 0
+    EXPECT_GE(randomNumber, 0); // expect random number to be >= 0
     EXPECT_TRUE(multiplication->stringify() == ("(" + to_string(randomNumber) + " * " + to_string(10.000000) + ")")); // expect stringify function return to = string of equation
 
     // deallocations
@@ -32,12 +32,12 @@ TEST(RandTest, RandMultTest) {
 	
 }
 
-// test case for random number being greater than or equal to 1
-TEST(RandTest, RandGreaterOrEqualOne) {
+// test case for random number being greater than or equal to 0
+TEST(RandTest, RandGreaterOrEqualZero) {
     // create rand object and assign to pointer
     Rand *test = new Rand();
     double randomNumber = test->evaluate(); // set variable equal to random generated number
-    EXPECT_GE(randomNumber, 1); // expect random number to be greater than or equal to 1
+    EXPECT_GE(randomNumber, 0); // expect random number to be greater than or equal to 0
     EXPECT_TRUE(test->stringify() == to_string(randomNumber)); // expect stringify function return to = string of equation
 	
     // deallocation	
@@ -45,18 +45,35 @@ TEST(RandTest, RandGreaterOrEqualOne) {
 	
 }
 
-// test case for random number being less than or equal to 100
-TEST(RandTest, RandLessEqualHundred) {
+// test case for random number being less than or equal to 99
+TEST(RandTest, RandLessEqualNinetyNine) {
     // create rand object and assign to pointer
     Rand *test = new Rand();
 
     double randomNumber = test->evaluate(); // set variable equal to random generated number
-    EXPECT_LE(randomNumber, 100); // expect random number to be less than or equal to 100
+    EXPECT_LE(randomNumber, 99); // expect random number to be less than or equal to 99
     EXPECT_TRUE(test->stringify() == to_string(randomNumber)); // expect stringify function return to = string of equation
 
     // deallocation
     delete test;
 }
+
+// testing that random numbers generated are not equal to each other
+TEST(RandTest, RandNotEqualsTest) {
+    // create rand objects and assign to pointer
+    Rand *num1 = new Rand();
+    Rand *num2 = new Rand();
+
+    double randomNumber1 = num1->evaluate(); // set variables equal to random generated number
+    double randomNumber2 = num2->evaluate();
+
+    EXPECT_NE(randomNumber1, randomNumber2); // expect random numbers not to be equal to each other
+
+    // deallocation
+    delete num1;
+    delete num2;
+}
+
 
 #endif //__RAND_TEST_HPP__
 
